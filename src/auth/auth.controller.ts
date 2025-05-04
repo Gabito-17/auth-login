7f9f0fd1cebf801ea0117f8daab0a7be01199b7e
XGabito12 <juangabrielpavon@gmail.com>
2025-05-04 20:44:55 -0300

    user-role.guards y creacion de lore-protected.decorator

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,18 @@
-import { Body, Controller, Get, Post, Req, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Req,
+  SetMetadata,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { GetUser, RawHeaders } from './decorators';
 import { CreateUserDto, LoginUserDto } from './dto';
 import { User } from './entities/user.entity';
+import { UserRoleGuard } from './guards/user-role/user-role.guard';
 
 @Controller('auth')
 export class AuthController {
@@ -19,6 +28,7 @@ export class AuthController {
   }
 
   @Get('private')
+  @SetMetadata('roles', ['admin', 'super-user'])
   @UseGuards(AuthGuard())
   testingPrivateRoute(
     @Req() request: Express.Request,
@@ -31,6 +41,16 @@ export class AuthController {
       message: 'anda todo',
       user: user,
       rawHeaders,
+    };
+  }
+
+  @Get('private2')
+  @SetMetadata('roles', ['admin', 'super-user'])
+  @UseGuards(AuthGuard(), UserRoleGuard)
+  testingPrivateRoute2(@Req() request: Express.Request) {
+    return {
+      ok: true,
+      message: 'anda todo',
     };
   }
 }
